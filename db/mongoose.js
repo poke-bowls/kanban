1d5da947144deec97b71467ad4de724ad21b9156
Micah Koki <micah.koki@gmail.com>
2016-02-15 13:47:19 -1000

    Updated Code

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,17 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/kanban');
+
+var uristring =
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/kanban';
+
+mongoose.connect(uristring, function(err, res){
+  if (err) {
+    console.log('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log('Succeeded connected to: ' + uristring);
+  }
+});
 
 var db =mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
